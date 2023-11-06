7bec572471c38bad076581ee36e509ba581c4c97
tcodes317 <tcodes317@gmail.com>
2023-11-06 14:21:58 -0800

    iii

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    screens:{
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
